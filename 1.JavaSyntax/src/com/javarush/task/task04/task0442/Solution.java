4e0b04c81d924850b9a2f84a86b687c5fa082a09
Mikhail Nemtsev <michael.nemtsev@mail.ru>
2018-12-23 00:45:06 +0300

    level 4 chapter 16

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task04.task0442;
+
+
+/* 
+Суммирование
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            int num = scanner.nextInt();
+            sum += num;
+            if (num == -1)
+                break;
+        }
+        System.out.println(sum);
+    }
+}
