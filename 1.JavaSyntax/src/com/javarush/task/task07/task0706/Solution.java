3b25152b1cc43f00e1bfe3ad328093a46a0d152f
Mikhail Nemtsev <michael.nemtsev@mail.ru>
2018-12-30 22:57:24 +0300

    level 7 chapter 4

--- 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task07.task0706;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Улицы и дома
@@ -11,5 +12,25 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        int[] arr = new int[15];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int cnt_ch = 0;
+        int cht_nech = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if ((i+10)%2 == 0)
+                cnt_ch += arr[i];
+            else
+                cht_nech += arr[i];
+        }
+
+        if (cnt_ch > cht_nech)
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        else if (cnt_ch < cht_nech)
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
     }
 }
